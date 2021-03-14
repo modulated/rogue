947c4bf34e756cd31ea61d09e89a99a0f6465119
modulated <cowkingdom@hotmail.com>
2021-03-14 23:28:03 +1100

    dla map working

--- src/map_builders/mod.rs
+++ src/map_builders/mod.rs
@@ -20,6 +20,9 @@ use drunkards_walk::DrunkardsWalkBuilder;
 mod maze;
 #[allow(unused_imports)]
 use maze::MazeBuilder;
+mod dla;
+#[allow(unused_imports)]
+use dla::DLABuilder;
 
 pub trait MapBuilder {
 	fn build_map(&mut self);
@@ -32,7 +35,7 @@ pub trait MapBuilder {
 
 pub fn random_builder(new_depth: i32) -> Box<dyn MapBuilder> {
 	let mut rng = rltk::RandomNumberGenerator::new();
-	let builder = rng.roll_dice(1, 3);
+	let builder = rng.roll_dice(1, 12);
 	match builder {
 		1 => Box::new(BspDungeonBuilder::new(new_depth)),
 		2 => Box::new(BspInteriorBuilder::new(new_depth)),
@@ -40,8 +43,12 @@ pub fn random_builder(new_depth: i32) -> Box<dyn MapBuilder> {
 		4 => Box::new(DrunkardsWalkBuilder::open_area(new_depth)),
 		5 => Box::new(DrunkardsWalkBuilder::open_halls(new_depth)),
 		6 => Box::new(DrunkardsWalkBuilder::winding_passages(new_depth)),
-	 	7 => Box::new(MazeBuilder::new(new_depth)),
+		7 => Box::new(MazeBuilder::new(new_depth)),
+		8 => Box::new(DLABuilder::walk_inwards(new_depth)),
+		9 => Box::new(DLABuilder::walk_outwards(new_depth)),
+		10 => Box::new(DLABuilder::central_attractor(new_depth)),
+		11 => Box::new(DLABuilder::insectoid(new_depth)),
 		_ => Box::new(SimpleMapBuilder::new(new_depth))
 	}
-	// Box::new(MazeBuilder::new(new_depth))
+	// Box::new(DLABuilder::new(new_depth))
 }
